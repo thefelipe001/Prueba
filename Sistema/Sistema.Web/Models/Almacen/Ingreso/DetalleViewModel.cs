6c90e432dbe771d37a369d76e269d7655f6248a9
thefelipe001 <61038817+thefelipe001@users.noreply.github.com>
2022-06-08 23:44:24 -0400

    fist

---
+++ Sistema/Sistema.Web/Models/Almacen/Ingreso/DetalleViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sistema.Web.Models.Almacen.Ingreso
+{
+    public class DetalleViewModel
+    {
+        [Required]
+        public int idarticulo { get; set; }
+        public string articulo { get; set; }
+        [Required]
+        public int cantidad { get; set; }
+        [Required]
+        public decimal precio { get; set; }
+    }
+}
